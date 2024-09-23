048061104efec408e8837187361a64fe8207a204
sasagz_escola <163011955+sasagz@users.noreply.github.com>
2024-09-23 12:04:36 -0300

    .

--- principal.js
+++ principal.js
@@ -1,6 +1,13 @@
 //IMC Paulo
+var título = document.querySelector(".título");
+título.textContent = "Aparecida Nutricionista";
+
 var Paulo = document.querySelector('#linhaPaulo');
-var pesoPaulo = Paulo.querySelector(",info-peso").textContent;
-var alturaPaulo = Paulo.querySelector(".info-altura").textContent;
+var pesoPaulo = parseFloat(Paulo.querySelector('.info-peso').textContent);
+var alturaPaulo = parseFloat(Paulo.querySelector('.info-altura').textContent);
+
+// Se alturaPaulo estiver em centímetros, divida por 100
+alturaPaulo = alturaPaulo / 100;
+
 var imcPaulo = pesoPaulo / (alturaPaulo * alturaPaulo);
-Paulo.querySelector(".info-imc").textContent = imcPaulo;
+Paulo.querySelector('.info-imc').textContent = imcPaulo.toFixed(2);
